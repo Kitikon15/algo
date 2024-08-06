d45210c7cba2e69518ad0f657208a1ba0d9f6c4c
Kitikon15 <674259003@webmail.npru,ac.th>
2024-08-06 10:42:32 +0700

    first commit

---
+++ Celsius0.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Celsius0 {
+    public static void main(String[]args){
+        // create a Scanner object for input
+        Scanner scanner = new Scanner(System.in);
+        int mySalary = 2000;
+
+        // Prompt the user for the temperature in Fahrenheit
+        System.out.print("Enter Fahrenheit degree: ");
+        Float fahrenheit = scanner.nextFloat();
+
+        // Convert Fahrenheit to Celsius
+        float celsius =(fahrenheit - 32)*5/9;
+
+        // Round the result to 2 decimal places
+        double roundedCelsius = Math.round(celsius * 100.0)/100.0;
+
+        // Display the result
+        System.out.println(fahrenheit+ "Fahrenheit is equal to" + roundedCelsius + "Celsius.");
+        
+        // Close the scanner
+        scanner.close();
+    }
+}
