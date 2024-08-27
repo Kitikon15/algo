ec6a00448cf5dc38aa4e9ac9dee3a8a0e99d8d3b
Kitikon15 <674259003@webmail.npru,ac.th>
2024-08-27 08:00:02 +0700

    new

---
+++ circleArea.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class circleArea {
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+        int radius = 5;
+        double circleArea = 3.14159*(radius*radius);
+        System.out.println("Enter radius="+radius);
+        System.out.println("Area of circle that has radius ="+radius +"meters is " +circleArea+ "square meters");
+        scanner.close();
+    }
+}
---
+++ salaryTax.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class salaryTax {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int salary = 30000;
+        int taxRate = 10;
+        int salarytax =(salary*10)/100;
+        System.out.println("ภาษีของเงินเดือน ="+salarytax);
+        System.out.println("เงินเดือน =" +salary);
+        scanner.close(); 
+    }
+}
